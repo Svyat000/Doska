f505e8afeb645180b33929c941f27dcd62c85745
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-02-20 16:35:53 +0300

    refactoring DialogHelper

--- app/src/main/java/com/sddrozdov/doska/dialogHelper/DialogHelper.kt
+++ app/src/main/java/com/sddrozdov/doska/dialogHelper/DialogHelper.kt
@@ -1,5 +1,6 @@
 package com.sddrozdov.doska.dialogHelper
 
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.sddrozdov.doska.MainActivity
 import com.sddrozdov.doska.R
@@ -13,13 +14,41 @@ class DialogHelper(private val act: MainActivity) {
 
     private val accountHelper = AccountHelper(act)
 
-
     fun createSignDialog(index: Int) {
         val builderDialog = AlertDialog.Builder(act)
         _binding = SignDialogBinding.inflate(act.layoutInflater)
 
         builderDialog.setView(binding.root)
+        setTextTitleAndButtonInDialog(index, binding)
 
+        val dialog = builderDialog.create()
+
+        binding.signDialogSingUpAndInButton.setOnClickListener {
+            takeFromTheFieldsEmailAndPassword(index, binding, dialog)
+        }
+        dialog.show()
+    }
+
+    private fun takeFromTheFieldsEmailAndPassword(
+        index: Int,
+        binding: SignDialogBinding,
+        dialog: AlertDialog
+    ) {
+        dialog.dismiss()
+        if (index == DialogConstants.SIGN_UP_STATE) {
+            accountHelper.signUpWithEmailAndPassword(
+                binding.signDialogEnterEmail.text.toString(),
+                binding.signDialogEnterPassword.text.toString()
+            )
+        } else {
+            accountHelper.signInWithEmailAndPassword(
+                binding.signDialogEnterEmail.text.toString(),
+                binding.signDialogEnterPassword.text.toString()
+            )
+        }
+    }
+
+    private fun setTextTitleAndButtonInDialog(index: Int, binding: SignDialogBinding) {
         if (index == DialogConstants.SIGN_UP_STATE) {
             binding.signDialogTitle.text = act.resources.getString(R.string.account_register)
             binding.signDialogSingUpAndInButton.text =
@@ -28,26 +57,7 @@ class DialogHelper(private val act: MainActivity) {
             binding.signDialogTitle.text = act.resources.getString(R.string.account_login)
             binding.signDialogSingUpAndInButton.text =
                 act.resources.getString(R.string.sign_dialog_sign_in_button)
+            binding.signDialogForgetPasswordButton.visibility = View.VISIBLE
         }
-
-
-        val dialog = builderDialog.create()
-
-        binding.signDialogSingUpAndInButton.setOnClickListener {
-            dialog.dismiss()
-            if (index == DialogConstants.SIGN_UP_STATE) {
-                accountHelper.signUpWithEmailAndPassword(
-                    binding.signDialogEnterEmail.text.toString(),
-                    binding.signDialogEnterPassword.text.toString()
-                )
-            } else {
-                accountHelper.signInWithEmailAndPassword(
-                    binding.signDialogEnterEmail.text.toString(),
-                    binding.signDialogEnterPassword.text.toString()
-                )
-            }
-
-        }
-        dialog.show()
     }
 }
