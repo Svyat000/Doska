9e1ef98858e83b60b1f11b3b7c08646111f5df12
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-03-19 17:43:24 +0300

    add icon, dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.material.v190)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.exifinterface)
