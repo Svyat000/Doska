0d95e24cfe6b6c75e64b3b5ab2cfd110f0ccbfcd
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-03-28 21:19:12 +0300

    ref ProgressDialog

--- app/src/main/java/com/sddrozdov/doska/dialogs/ProgressDialog.kt
+++ app/src/main/java/com/sddrozdov/doska/dialogs/ProgressDialog.kt
@@ -5,22 +5,13 @@ import androidx.appcompat.app.AlertDialog
 import com.sddrozdov.doska.databinding.ProgressDialogLayoutBinding
 
 object ProgressDialog {
-
-    private var _binding: ProgressDialogLayoutBinding? = null
-    private val binding get() = _binding ?: throw IllegalStateException("Binding must not be null")
-
-    fun createProgressDialog(activity: Activity) : AlertDialog {
-
+    fun createProgressDialog(activity: Activity): AlertDialog {
+        val binding = ProgressDialogLayoutBinding.inflate(activity.layoutInflater)
         val builderDialog = AlertDialog.Builder(activity)
-        _binding = ProgressDialogLayoutBinding.inflate(activity.layoutInflater)
         builderDialog.setView(binding.root)
-
         val dialog = builderDialog.create()
-
         dialog.setCancelable(false)
-
         dialog.show()
-
         return dialog
     }
 }
