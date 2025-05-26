6d748b56405b66faef82e79e9f18b505c62c4f7b
Svyatoslav Drozdov <sddrozdov000@gmail.com>
2025-05-26 13:45:49 +0300

    ref MessageAdapter

--- app/src/main/java/com/sddrozdov/doska/recyclerViewAdapters/MessageAdapter.kt
+++ app/src/main/java/com/sddrozdov/doska/recyclerViewAdapters/MessageAdapter.kt
@@ -4,12 +4,26 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseAuth
 import com.sddrozdov.doska.databinding.ChatItemBinding
 import com.sddrozdov.doska.models.Message
 
 class MessageAdapter :
     androidx.recyclerview.widget.ListAdapter<Message, MessageAdapter.ItemHolder>(ItemComparator()) {
+
+    companion object {
+        private const val VIEW_TYPE_MESSAGE_SENT = 1
+        private const val VIEW_TYPE_MESSAGE_RECEIVED = 2
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val message = getItem(position)
+        return if (message.senderId == FirebaseAuth.getInstance().currentUser?.uid) {
+            VIEW_TYPE_MESSAGE_SENT
+        } else {
+            VIEW_TYPE_MESSAGE_RECEIVED
+        }
+    }
 
     class ItemHolder(val binding: ChatItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(message: Message) = binding.apply {
@@ -31,15 +45,15 @@ class MessageAdapter :
     }
 
     class ItemComparator : DiffUtil.ItemCallback<Message>() {
-        override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
-            return oldItem == newItem
-        }
 
+        override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
+            return oldItem.timestamp == newItem.timestamp
+        }
         override fun areContentsTheSame(oldItem: Message, newItem: Message): Boolean {
-            return oldItem == newItem
+            return oldItem.text == newItem.text &&
+                    oldItem.senderId == newItem.senderId
         }
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
         return ItemHolder.create(parent)
     }
